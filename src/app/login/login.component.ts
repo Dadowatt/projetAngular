1bd22aef006e242928ccd17cafb6c6dc8f73121f
Dado Watt <dadowatt92@gmail.com>
2024-11-04 11:43:00 +0000

    validation de formulaire

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl,ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  // Validators.required
+  username= new FormControl('', [Validators.required,Validators.minLength(3), Validators.maxLength(20)])
+  password= new FormControl('', Validators.required,)
 
+  OnSubmit(Event:Event){
+    Event.preventDefault()
+    console.log(this.username.value, this.password.value);
+    
+  }
 }
